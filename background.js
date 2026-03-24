a1a2c749737cd9dfd684e2f10fab192e17f2a60b
fawaddev0 <fawad.imran.000@gmail.com>
2026-03-24 10:57:13 +0500

    storing transcript locally to process it after recording ends

--- background.js
+++ background.js
@@ -17,12 +17,24 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.type === 'START_RECORDING') {
     const streamId = await chrome.tabCapture.getMediaStreamId();
     const { deepgramApiKey } = await chrome.storage.local.get("deepgramApiKey");
+    console.log(deepgramApiKey)
     chrome.runtime.sendMessage({ type: "INIT", streamId, deepgramApiKey })
   }
 
 
   if (message.type === 'STOP_RECORDING') {
     chrome.runtime.sendMessage({ type: "STOP" })
+  }
+
+  if (message.type === 'SAVE_TRANSCRIPT') {
+    const existing = await chrome.storage.local.get("transcript");
+    const prev = existing.transcript || "";
+    console.log("[STORAGE] Previous: ", prev);
+    
+    await chrome.storage.local.set({ 
+      transcript: prev + " " + message.transcriptSnapshot.join(" ")
+    });
+    console.log("[STORAGE] Transcript saved")
   }
 });
 
