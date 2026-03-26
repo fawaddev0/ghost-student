986dd9592f47d0ae01ac87915fcac316d55a8f76
fawaddev0 <fawad.imran.000@gmail.com>
2026-03-26 09:09:22 +0500

    sending transcript to n8n workflow to generate notes and save them into notion

--- background.js
+++ background.js
@@ -15,15 +15,31 @@ chrome.offscreen.hasDocument().then(async(existing) => {
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.type === 'START_RECORDING') {
+    // Set recording state to true
+    await chrome.storage.local.set({ isRecording: true })
+
+    // Fetch stream id to start transcription
     const streamId = await chrome.tabCapture.getMediaStreamId();
     const { deepgramApiKey } = await chrome.storage.local.get("deepgramApiKey");
-    console.log(deepgramApiKey)
     chrome.runtime.sendMessage({ type: "INIT", streamId, deepgramApiKey })
   }
 
 
   if (message.type === 'STOP_RECORDING') {
     chrome.runtime.sendMessage({ type: "STOP" })
+    
+    // Send transcription to n8n for generating and storing notes
+    const existing = await chrome.storage.local.get("transcript");
+    await fetch("https://n8n.fawad.live/webhook/generate-notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ transcript: existing.transcript })
+    })
+
+    // Remove all data stored in storage
+    await chrome.storage.local.remove(["isRecording", "transcript"]);
   }
 
   if (message.type === 'SAVE_TRANSCRIPT') {
